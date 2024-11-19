3d5dac9cce0146621bf6410bcced5a9a1774e4ad
hongky06 <dohongky2006@gmail.com>
2024-11-19 14:11:21 +0700

    Add files via upload

---
+++ ss2 b4.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int canh = 0 ; 
+   
+    scanf ("%d", &canh);
+	
+	int chu_vi = canh * 4;	
+	printf("chu vi cua hinh vuong la %d \n ",chu_vi);
+	 
+	int dien_tich = canh * canh ; 
+	printf("dien tich cua hinh vuong la %d \n ",dien_tich);
+	
+	
+	
+	
+	
+	
+	return 0; 
+	
+	
+	
+	
+	
+	
+}
