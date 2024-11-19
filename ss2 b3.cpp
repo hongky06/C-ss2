dbe7c6bf07ac75050d826af89b4a392e0f5054b4
hongky06 <dohongky2006@gmail.com>
2024-11-19 13:36:36 +0700

    Add files via upload

---
+++ ss2 b3.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int num1 = 15 ;
+	int num2 =5;
+    
+	
+	int hieu = num1 - num2; 
+	printf ("hieu la %d \n",hieu);
+	 
+	int tich = num1 * num2; 
+    printf ("tich la %d \n",tich);
+	
+	int thuong = num1 / num2; 
+	printf ("thuong la %d \n",thuong);
+		return 0; 
+	
+	
+}
